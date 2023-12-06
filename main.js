35ec2b85752564f8dd31dad044e6db7d883e11aa
harangtuesday <117243962+harangtuesday@users.noreply.github.com>
2023-12-06 11:51:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('table', {
+    res.render('/home', {
         user: req.user
     });
 });
